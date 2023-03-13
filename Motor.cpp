1a66a4c0c8c3db9c4778708d2e8c028ee560081b
Maxwell Jay <MaxwellJay@1fsc7p.onmicrosoft.com>
2023-03-13 23:09:21 +0800

    修改Ti

--- Motor.cpp
+++ Motor.cpp
@@ -63,7 +63,7 @@ void Motor::GetEncoder() {
     }
 }
 
-const float Kp = 10, Ti = 5, Td = 5, T = period; //原先为5 140 80 
+const float Kp = 10, Ti = 20, Td = 5, T = period; //原先为5 140 80 
 int Motor::PIDControl(double target)
 {
     //离散增量式PID
