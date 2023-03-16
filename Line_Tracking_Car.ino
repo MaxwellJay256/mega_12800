48f6159ca02717a638afdd41af93fba044ca97bc
Maxwell Jay <MaxwellJay@1fsc7p.onmicrosoft.com>
2023-03-16 20:48:31 +0800

    新增心跳灯

--- Line_Tracking_Car.ino
+++ Line_Tracking_Car.ino
@@ -6,6 +6,8 @@
 
 void Control() {
     Drive::Patrol();
+    // MotorR.Spin(-20);
+    // MotorL.Spin(-10);
 }
 
 bool ObstacleAvoidFlag = true;
@@ -14,6 +16,7 @@ void setup()
 {
     //PWM频率调节，设置9、10引脚的PWM输出频率为31372Hz，适合于我们使用的电机
     TCCR1B = TCCR1B & B11111000 | B00000001;
+    pinMode(LED_BUILTIN, OUTPUT);
     DeviceInit();
     RobotArm::ClawDown();
     RobotArm::ClawUp();
@@ -23,13 +26,25 @@ void setup()
 
 void loop()
 {
-    DisplayInfo();
+    HeartBeat();
+    // DisplayInfo();
     /*/ 只执行一次超声波避障
     if ( ObstacleAvoidFlag ) {
         Drive::ObstacleAvoidace();
         ObstacleAvoidFlag = false;
+    } else {
+        Drive::PatrolEnd();
     }
     //*/
-    //Drive::PatrolEnd();
     delay(period);
+}
+
+bool heartBeatFlag = false;
+void HeartBeat() {
+    if ( heartBeatFlag ) {
+        digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+    heartBeatFlag = !heartBeatFlag;
 }
