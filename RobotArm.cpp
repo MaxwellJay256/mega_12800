8175f80d7a9e83e8f1d4d0ff8c7c601b603db610
Maxwell Jay <MaxwellJay@1fsc7p.onmicrosoft.com>
2023-03-18 00:22:03 +0800

    修改机械臂预定位置

--- RobotArm.cpp
+++ RobotArm.cpp
@@ -1,8 +1,8 @@
 #include "RobotArm.h"
-#define LIFT_DOWN_POSITION 70
-#define LIFT_UP_POSITION 100
+#define LIFT_DOWN_POSITION 74
+#define LIFT_UP_POSITION 135
 #define CLAW_FREE_POSITION 90
-#define CLAW_HOLD_POSITION 120
+#define CLAW_HOLD_POSITION 125
 
 namespace RobotArm
 {
@@ -12,13 +12,11 @@ namespace RobotArm
         Claw.write(CLAW_HOLD_POSITION);
         delay(700);
         Lift.write(LIFT_UP_POSITION);
-        delay(1000);
     }
 
     void ClawDown() {
         Lift.write(LIFT_DOWN_POSITION);
-        delay(1000);
+        delay(900);
         Claw.write(CLAW_FREE_POSITION);
-        delay(1000);
     }
 }
